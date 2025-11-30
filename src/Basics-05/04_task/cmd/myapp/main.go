1bed3f472f15873cde7accad48f52305c9857a28
Dmitrii Starikov <dd.starikov@gmail.com>
2025-11-30 13:47:00 +0300

    empty interfaces and structs task

---
+++ src/Basics-05/04_task/cmd/myapp/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"myapp/internal"
+)
+
+type Printer interface {
+	print()
+}
+
+type A struct {
+}
+
+func (a *A) print() {
+	fmt.Println("Hello, playground")
+}
+
+func main() {
+	cust := internal.NewCustomer("Johnny", 23, 10000, 1000, true)
+	_ = startTransaction(cust)
+	fmt.Printf("%+v\n", cust)
+
+	partner := internal.NewPartner("Donald", 23, 10000, 1000)
+	_ = startTransaction(partner)
+	fmt.Printf("%+v\n", partner)
+
+	err := startTransactionDynamic(partner)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%+v\n", partner)
+	}
+
+	err = startTransactionDynamic(123)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func startTransaction(debtor internal.Debtor) error {
+	return debtor.WrOffDebt()
+}
+
+func startTransactionDynamic(d interface{}) error {
+	debtor, ok := d.(internal.Debtor)
+
+	if ok {
+		return startTransaction(debtor)
+	}
+
+	return errors.New("incorrect type")
+}
